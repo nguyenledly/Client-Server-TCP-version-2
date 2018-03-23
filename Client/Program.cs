b07b3b340e410c5383df69f6280e6f52f8aaaeb3
TuanNguyen <TuanNguyen@HOANGTUAN-PC>
2018-03-23 20:40:19 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            TcpClient client = new TcpClient();
+            client.Connect(iep);
+            NetworkStream ns = client.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            string s = sr.ReadLine();
+            Console.WriteLine(s);
+            sw.WriteLine("Hello Server!");
+            sw.Flush();
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 1234);
+            TcpListener server = new TcpListener(ipe);
+            server.Start();
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            sw.WriteLine("Hello Client!");
+            sw.Flush();
+            StreamReader sr = new StreamReader(ns);
+            string s = sr.ReadLine();
+            Console.WriteLine(s);
+            client.Close();
+            server.Stop();
+        }
+    }
+}
